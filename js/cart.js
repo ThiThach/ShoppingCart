a278fe55ca420530d33a5257a0b9ed35da1a235a
Thi Thach <thithach@hotmail.com>
2013-10-11 18:12:50 +1300

    Add RequireJS
    Add RequireJS to reduce javascript file references

--- js/cart.js
+++ js/cart.js
@@ -1,39 +1,43 @@
-window.TShoppingCart =  window.TShoppingCart || {};
+require( ['models/product'], function() {
 
-window.TShoppingCart.Cart = function(data) {
-	var self = this;
+	window.TShoppingCart =  window.TShoppingCart || {};
 	
-	self.items = ko.observableArray([]);
-	self.isEmpty = ko.computed(function(){
-		return (self.items().length == 0);
-	});
-	
-	self.add = function(item) {
-		var purchasedItem = ko.utils.arrayFirst(self.items(), function(obj){
-			return (obj.id === item.id)
+	window.TShoppingCart.Cart = function(data) {
+		var self = this;
+		
+		self.items = ko.observableArray([]);
+		self.isEmpty = ko.computed(function(){
+			return (self.items().length == 0);
 		});
 		
-		if (purchasedItem === null) {
-			self.items.push(item);
+		self.add = function(item) {
+			var purchasedItem = ko.utils.arrayFirst(self.items(), function(obj){
+				return (obj.id === item.id)
+			});
 			
-			//if user change the bought quantity to zero
-			//then we need to remove this item off the cart
-			item.bought.subscribe(function(newValue){
-				if (parseInt(newValue) === 0) {
-					self.items.remove(item);
+			if (purchasedItem === null) {
+				self.items.push(item);
+				
+				//if user change the bought quantity to zero
+				//then we need to remove this item off the cart
+				item.bought.subscribe(function(newValue){
+					if (parseInt(newValue) === 0) {
+						self.items.remove(item);
+					}
+				});
+			}
+		};
+		
+		self.subtotal = ko.computed(function(){
+			var total = 0;
+			ko.utils.arrayForEach(self.items(), function(item){
+				if(item.bought()) {
+					total += item.total();
 				}
 			});
-		}
-	};
-	
-	self.subtotal = ko.computed(function(){
-		var total = 0;
-		ko.utils.arrayForEach(self.items(), function(item){
-			if(item.bought()) {
-				total += item.total();
-			}
+			
+			return total;
 		});
-		
-		return total;
-	});
-};
+	};
+
+});
--- js/main.js
+++ js/main.js
@@ -1,57 +1,68 @@
-window.TShoppingCart =  window.TShoppingCart || {};
+require( ['cart'], function( ) {
 
-window.TShoppingCart.App = function(data) {
-	var self = this;
+	window.TShoppingCart =  window.TShoppingCart || {};
+	window.TShoppingCart.App = function(data) {
+		var self = this;
+		
+		self.items = ko.observableArray([]);
+		self.cart = new window.TShoppingCart.Cart();
+		
+		self.init = function(){
+			var items = [
+						{id:1,name:'Milk',price:'1.75',stock:10},
+						{id:2,name:'Sugar',price:'2.50',stock:20},
+						{id:3,name:'Coffee',price:'12.99',stock:50} 
+						];
+						
+						
+			for (var i=0, len=items.length; i<len; i++){
+				var item = new TShoppingCart.Product(items[i]);
+				self.items().push(item);
+			}
+		};
+		
+		self.buy = function(item) {
+			self.cart.add(item);
+			item.buy();	
+		};
 	
-	self.items = ko.observableArray([]);
-	self.cart = new window.TShoppingCart.Cart();
-	
-	self.init = function(){
-		var items = [
-					{id:1,name:'Milk',price:'1.75',stock:10},
-					{id:2,name:'Sugar',price:'2.50',stock:20},
-					{id:3,name:'Coffee',price:'12.99',stock:50} 
-					];
-					
-					
-		for (var i=0, len=items.length; i<len; i++){
-			var item = new TShoppingCart.Product(items[i]);
-			self.items().push(item);
-		}
+		//------ Thi codes --------
+		self.selectedItem = ko.observable(null);
+		self.selectItem = function() {
+			self.selectedItem(this);		
+		};
+		
+		self.newItem = function() {
+			var item = new TShoppingCart.Product({id:0,name:'new product',price:'1.99',stock:1});
+			self.selectedItem(item);
+		};
+		
+		self.saveItem = function() {
+			var currentItem = self.selectedItem();
+			var isNew = (currentItem && currentItem.id() === 0);
+			currentItem.save();
+			
+			if (isNew){
+				var obj = {id:self.items().length+1,name:currentItem.name(),price:currentItem.price(),stock:currentItem.stock()};
+				var item = new TShoppingCart.Product(obj);
+				self.items.push(item);
+			}
+			
+			self.cancelItem();
+		};	 
+		
+		self.cancelItem = function(){
+			self.selectedItem(null);
+		}	
 	};
 	
-	self.buy = function(item) {
-		self.cart.add(item);
-		item.buy();	
-	};
+	//Initalise new shopping cart
+	setTimeout(function(){
+		var app = new window.TShoppingCart.App();
+		app.init();
+		ko.applyBindings(app);	
+	}, 400);
+	
 
-	//------ Thi codes --------
-	self.selectedItem = ko.observable(null);
-	self.selectItem = function() {
-		self.selectedItem(this);		
-	};
-	
-	self.newItem = function() {
-		var item = new TShoppingCart.Product({id:0,name:'new product',price:'1.99',stock:1});
-		self.selectedItem(item);
-	};
-	
-	self.saveItem = function() {
-		var currentItem = self.selectedItem();
-		var isNew = (currentItem && currentItem.id() === 0);
-		currentItem.save();
-		
-		if (isNew){
-			var obj = {id:self.items().length+1,name:currentItem.name(),price:currentItem.price(),stock:currentItem.stock()};
-			var item = new TShoppingCart.Product(obj);
-			self.items.push(item);
-		}
-		
-		self.cancelItem();
-	};	 
-	
-	self.cancelItem = function(){
-		self.selectedItem(null);
-	}	
-	
-};
+});
+
